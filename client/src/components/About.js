1d8ffd0da47a286a9da9f1c7c3c56caf292f67aa
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-04-16 17:17:06 +0530

    [Bug] : #9 team-mates info card is not styled good

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import '../style/about.css';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function About() {
   const teamMembers = [
@@ -14,65 +20,70 @@ function About() {
       id: 1,
       name: "Khaja Khadeerullah",
       email: "khadeer@gamil.com",
-      image: "https://avatars.githubusercontent.com/u/85341844?v=4", 
+      image: "https://avatars.githubusercontent.com/u/85341844?v=4",
       github: "https://github.com/khadeerthewizard",
     },
     {
       id: 2,
       name: "Sameer Shaik",
       email: "sameer.kattubadi@gamil.com",
-      image: "https://avatars.githubusercontent.com/u/112153702?v=4", 
+      image: "https://avatars.githubusercontent.com/u/112153702?v=4",
       github: "https://github.com/SSameer20",
     },
     {
       id: 3,
       name: "Shaik Sohail",
       email: "s.suhail9849@gmail.com",
-      image: "https://avatars.githubusercontent.com/u/122728924?v=4", 
+      image: "https://avatars.githubusercontent.com/u/122728924?v=4",
       github: "https://github.com/Sohail-9",
     },
   ];
 
   return (
     <div className="about">
+      <h2 style={{marginTop:"100px"}}>Meet the Team</h2>
       
+
       <div className="team">
-      <h2>Meet the Team</h2>
-        {teamMembers.map((member) => (
-          <div className="member" key={member.id}>
-            {member.id % 2 === 0 ? ( // Check if id is even
+      
+
+
+
+        {
+          teamMembers.map((member) => (
+            <div className="member" key={member.id}>
+
               <>
-                <div className="image">
-                  <img src={member.image} alt={member.name} />
-                </div>
-                <div className="information">
-                  <h2>{member.name}</h2>
-                  <p>{member.email}</p>
-                  <button>
-                    <a href={member.github} target="_blank">
-                      Profile
-                    </a>
-                  </button>
-                </div>
+
+
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image={member.image}
+                    title="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {member.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {member.email}
+                    </Typography>
+
+
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small"><a href={member.github} target="_blank" > Open Github</a></Button>
+                  </CardActions>
+                </Card>
+
+
               </>
-            ) : (
-              <>
-                <div className="information">
-                  <h2>{member.name}</h2>
-                  <p>{member.email}</p>
-                  <button>
-                    <a href={member.github} target="_blank">
-                      Profile
-                    </a>
-                  </button>
-                </div>
-                <div className="image">
-                  <img src={member.image} alt={member.name} />
-                </div>
-              </>
-            )}
-          </div>
-        ))}
+
+
+            </div>
+          ))}
       </div>
     </div>
   );
