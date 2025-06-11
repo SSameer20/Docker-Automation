57c139049067832a9b8d135d76d0543573cc3a40
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-06-11 22:41:15 +0530

    code and file cleanup to organize repo for better developer experience

--- server/src/__tests__/app.test.ts
+++
@@ -1,1 +0,0 @@
-// Check whether express app is running and responds to a request
--- server/src/__tests__/image.test.ts
+++
@@ -1,4 +0,0 @@
-// This is a simple test file to ensure that the testing framework is set up correctly.
-test("adds 2 + 3 = 5", () => {
-  expect(2 + 3).toBe(5);
-});
